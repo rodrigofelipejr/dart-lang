5b87763ba4dcaba204e19b79a51590fb287eb560
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-07-21 14:56:58 -0300

    Reduce - Fold

--- Fundamentos/16-Colecoes/list/list.dart
+++ Fundamentos/16-Colecoes/list/list.dart
@@ -8,7 +8,7 @@ import 'dart:math';
  */
 
 listForEach() {
-  print('List ForEach\n');
+  print('List --------------- ForEach\n');
 
   var array = [0, 2.5, 5, 7.25, 10];
   array.forEach((e) {
@@ -77,7 +77,7 @@ listForEach() {
  */
 
 listExpand() {
-  print('List Expand\n');
+  print('List --------------- Expand\n');
 
   var lista = [
     [1, 2],
@@ -106,7 +106,7 @@ listExpand() {
  */
 
 listMap() {
-  print('List Map\n');
+  print('List --------------- Map\n');
 
   List<String> frutas = ['Morango', 'Banana', 'Tomate'];
   List<String> frutasMapeadas = frutas.map((e) => '$e é uma fruta').toList();
@@ -136,7 +136,7 @@ listMap() {
  */
 
 listEvery() {
-  print('List - Every\n');
+  print('List --------------- Every\n');
 
   List<int> inteiros = [0, 1, 2, 3, 4, 5];
   bool resultado = inteiros.every((e) => e >= 0);
@@ -154,7 +154,7 @@ listEvery() {
  */
 
 listWhere() {
-  print('List - Where\n');
+  print('List --------------- Where\n');
 
   List<int> idades = [36, 12, 18, 31, 17, 23];
   print(idades.where((e) => e is int)); //NOTE - retorno em Iterable
@@ -164,7 +164,64 @@ listWhere() {
   var crianca = idades.singleWhere((e) => e < 12, orElse: () => 0);
   //NOTE - orElse => evita erros caso a condição não seja atendida
   int menor = idades.lastWhere((e) => e.toString().startsWith('0'), orElse: () => 0);
-  print('Adultos: $adultos - Criança: $crianca - Menor: $menor');
+  print('Adultos: $adultos - Criança: $crianca - Menor: $menor\n');
+}
+
+/**
+ * Conceito - Reduce
+ * - Compara os elementos da coleção, retornando 1 único elemento resultante
+ * - Ao interagir com os elementos o indice ) se torna o anterior, indice 1 se torna o atual.
+ */
+
+listReduce() {
+  print('List --------------- Reduce\n');
+  List<int> numerosPares = List.generate(6, (i) => i * 2);
+  print(numerosPares);
+
+  //NOTE - o retorno será o próximo anterior
+  int resultado = numerosPares.reduce((previous, current) {
+    print('Previous: $previous + Current: $current');
+    return previous + current;
+  });
+  print(resultado);
+
+  List<bool> boleanos = [true, false, !true, !false];
+  bool and = boleanos.reduce((ant, atu) => ant && atu);
+  bool or = boleanos.reduce((ant, atu) => ant || atu);
+  print('and: $and - or: $or');
+
+  List<int> idades = [36, 12, 18, 31, 17, 23];
+  int maisNovo = idades.reduce((ant, atu) => (ant > atu) ? atu : ant);
+  int maisVelho = idades.reduce((ant, atu) => (ant < atu) ? atu : ant);
+  int media = idades.reduce((a, c) => a + c);
+  print('mais novo: $maisNovo - mais velho: $maisVelho - média: ${(media / idades.length).toStringAsFixed(1)}\n');
+}
+
+/**
+ * Conceito - Fold
+ * - Compara os elementos da coleção, retornando 1 único elemento resultante
+ * - O valor inicial se torna o anterior e o indice 0 se torna o atual.
+ */
+
+listFold() {
+  print('List --------------- Fold\n');
+
+  List<int> numerosPares = List.generate(6, (i) => i * 2);
+  print(numerosPares);
+  int valorInicial = 10;
+  int resultado = numerosPares.fold(valorInicial, (previousValue, current) {
+    print('$previousValue + $current');
+    return previousValue + current;
+  });
+  print('$resultado\n');
+
+  print(numerosPares);
+  valorInicial = 30;
+  resultado = numerosPares.fold(valorInicial, (previous, current) {
+    print('$previous - $current');
+    return previous - current;
+  });
+  print('$resultado\n');
 }
 
 main() {
@@ -173,4 +230,6 @@ main() {
   listMap();
   listEvery();
   listWhere();
+  listReduce();
+  listFold();
 }
