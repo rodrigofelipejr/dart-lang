2ecc8ce11eaddd00385f07cedc9a6e653a5aa600
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-07-25 22:14:56 -0300

    Async - Futures

---
+++ Fundamentos/18-asynchronous-programming/1-feature/feature.dart
@@ -0,0 +1,38 @@
+/**
+ * Conceito - Feature
+ * - É um objeto que retorna um valor ou erro, resultado de uma execução assíncrona/future
+ */
+
+void countdown({int seconds = 3}) {
+  print('Loading...');
+  for (var i = 1; i <= seconds; i++) {
+    Future.delayed(Duration(seconds: i), () {
+      print('${i * 33}%');
+      if (i == seconds) print('\nComplete!');
+    });
+  }
+}
+
+String task([int time = 1]) {
+  var msg = status(time: time);
+  return 'Status $msg';
+}
+
+void newTask([int time = 1]) {
+  Future.delayed(Duration(seconds: time), () => print("Primeira tarefa finalizada!\n"));
+}
+
+Future<String> status({int time = 1}) {
+  return Future.delayed(Duration(seconds: time), () => "Segunda tarefa finalizada!\n");
+}
+
+void exceptionTask([int time = 1]) =>
+    Future.delayed(Duration(seconds: time), () => throw Exception('Erro intencional'));
+
+main() {
+  print('Features\n');
+  print(task(4));
+  newTask(4);
+  exceptionTask(5);
+  countdown();
+}
